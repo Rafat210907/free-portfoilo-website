42bc1fc83ef47cc69c9685e52617c9f7e90b51f6
rafat21 <0092powerstar@gmail.com>
2024-12-01 15:54:35 +0600

    contents resized

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,11 +12,11 @@ const Hero = () => {
       <div className="flex justify-center ml-2">
         <div className="lg:mt-28">
          <h1 className="text-green-700 text-2xl mb-2"> Welcome</h1>
-          <h1 className="text-6xl text-black mb-6">I have <span className="text-green-600 font-poppins">Creative <br /> Design</span> Experience</h1>
+          <h1 className="sm:text-6xl text-4xl text-black mb-6">I have <span className="text-green-600 font-poppins">Creative <br /> Design</span> Experience</h1>
           <p className="text-gray-900 text-xl mb-10">I’m Tanvir, a creative Product Designer. I’ve been helping businesses <br /> to solve their problems with my design for 2 years.</p>
 
         <div className="flex gap-10">
-            <button className="h-16 w-40 border-2 border-green-500 rounded-[12px] pointer bg-green-500 hover:text-black duration-200 text-xl" 
+            <button className="h-14 w-36 border-2 border-green-500 rounded-[12px] pointer bg-green-500 hover:text-black duration-200 text-lg" 
             onClick={() => setIsOpen(!isOpen)}>Contact Me</button>
             <button className="text-black text-lg hover:text-green-400  pointer hover:border-2 rounded-[10px] duration-600 hover:border-green-400 p-4">View Portfolio</button>
           </div>
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,10 +1,9 @@
 
 import { Link } from "react-router-dom";
-
 const Footer = () => {
   return (
     <footer className= "pb-4 relative max-lg:mt-32 mt-14">      
-      <div className="pt-5 border-t border-dark/20">
+      <div className="pt-5 border-t border-black/20">
         <p className="text-center text-[13px] text-black">
           Copyright © | Developed By{" "}
           <Link
