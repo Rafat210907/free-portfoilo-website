27b898266ffadd589a885e133e2ad3c6ebb1ee81
rafat21 <0092powerstar@gmail.com>
2024-12-02 18:20:51 +0600

    responsive ness

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -8,14 +8,14 @@ const ContactUsPage = () => {
     Aos.init();
   }, []);
     return (
-        <section id="contact" className="max-width">
-        <h6 data-aos='fade-right' data-aos-duration="700" className="text-lg sm:text-2xl text-center text-green-500 mb-1 sm:mb-2">
+        <section id="contact" className='max-width'>
+        <h6 className="text-lg sm:text-2xl text-center text-green-500 mb-1 sm:mb-2">
           Contact Me
         </h6>
-        <h2 data-aos='fade-left' data-aos-duration="700" className="text-2xl sm:text-4xl text-center font-bold mb-4 sm:mb-4">
+        <h2 className="text-2xl sm:text-4xl text-center font-bold mb-4 sm:mb-4">
         Let’s Discuss Your <span className='text-green-600'>Project</span>
         </h2>
-        <p className='text-center text-black/70 text-lg'>Let’s make something new, different and more meaningful or make <br /> thing more visual or conceptual</p>
+        <p className='text-center text-black/70 text-lg mb-6'>Let’s make something new, different and more meaningful or make <br /> thing more visual or conceptual</p>
         <ContactForm />
         </section>
     );
